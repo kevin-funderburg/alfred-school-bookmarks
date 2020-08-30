3b3e1876cf1791b5e6be04939748e7d28fbc3e40
Kevin Funderburg <ksfunderburg@icloud.com>
2020-08-29 20:50:45 -0500

    removing uneeded files

--- create semester table.sql
+++ create semester table.sql
@@ -1,33 +1,35 @@
+-- this is an example of the SQL I executed, change it to fit your needs
+
 CREATE TABLE "fall2020" (
-	"name"			TEXT NOT NULL,
+	"name"				TEXT NOT NULL,
 	"folderPath"		TEXT NOT NULL,
-	"bookPath"		TEXT,
-	"oneNote"		TEXT,
-	"website"		TEXT,
-	"zoom"			TEXT
+	"bookPath"			TEXT,
+	"oneNote"			TEXT,
+	"website"			TEXT,
+	"zoom"				TEXT
 );
 
 INSERT INTO fall2020 VALUES (
 	'Algorithm Design',
 	'~/Dropbox/School/algorithm design',
 	'~/Dropbox/School/algorithm design/Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein - Introduction to Algorithms 3rd Edition (2009).pdf',
-	'onenote:https://d.docs.live.net/9478a1a4ec3795b7/Documents/School/_Fall%202020/Algorithm%20Design/',
-	'https://canvas.txstate.edu/courses/1505754',
-	'https://txstate.zoom.us/j/91910138336?pwd=NHF6b1J6Q3dFN1RjejlWbk56Vmw5QT09'
+	'onenote:https://d.docs.live.net/somethingsomething
+	'https://class1 website',
+	'https://zoomlink'
 	);
 INSERT INTO fall2020 VALUES (
-	'Compilier Construction',  
-	'~/Dropbox/School/compiler construction', 
+	'Compilier Construction',
+	'~/Dropbox/School/compiler construction',
 	'~/Dropbox/School/compiler construction/Keith Cooper, Linda Torczon - Engineering a Compiler-Elsevier Science & Technology (2011).epub',
-	'onenote:https://d.docs.live.net/9478a1a4ec3795b7/Documents/School/_Fall%202020/Compilier%20Construction/', 
-	'https://canvas.txstate.edu/courses/1425910',
-	'https://txstate.zoom.us/j/92907307060?pwd=dVdMNjluRWhaT1NQd2VXRGYvN04vQT09'
+	'onenote:https://d.docs.live.net/somethingsomething',
+	'https://class2 website',
+	'https://zoomlink'
 	);
 INSERT INTO fall2020 VALUES (
-	'Computer Networks', 
-	'~/Dropbox/School/computer networks', 
-	'~/Dropbox/School/algorithm design/Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein - Introduction to Algorithms 3rd Edition (2009).pdf', 
-	'onenote:https://d.docs.live.net/9478a1a4ec3795b7/Documents/School/_Fall%202020/Computer%20Networks/', 
-	'https://keystone.cs.txstate.edu/personal/teaching/cs5310/f-20/index.html',
-	'994 6078 8734'
+	'Computer Networks',
+	'~/Dropbox/School/computer networks',
+	'~/Dropbox/School/algorithm design/Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein - Introduction to Algorithms 3rd Edition (2009).pdf',
+	'onenote:https://d.docs.live.net/somethingsomething',
+	'https://class3 website',
+	'https://zoomlink'
 	);
